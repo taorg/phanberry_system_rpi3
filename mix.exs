6a2bbf2546c70f0d52700cfe10fc53d2b912ef82
taorg <antonio.lopez@ptbsl.com>
2018-09-09 14:57:20 +0200

    phanberry_rpi3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
-defmodule NervesSystemRpi3.MixProject do
+defmodule PhanberrySystemRpi3.MixProject do
   use Mix.Project
 
-  @app :nerves_system_rpi3
+  @app :phanberry_rpi3
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
            |> String.trim()
@@ -63,10 +63,10 @@ defmodule NervesSystemRpi3.MixProject do
 
   defp package do
     [
-      maintainers: ["Frank Hunleth", "Justin Schneck"],
+      maintainers: ["Taorg", "Manolets"],
       files: package_files(),
       licenses: ["Apache 2.0"],
-      links: %{"Github" => "https://github.com/nerves-project/#{@app}"}
+      links: %{"Github" => "https://github.com/taorg/phanberry_system_rpi3"}
     ]
   end
 
